020923694b169892ba0ed8b6d4823e4877388358
Erik Hollensbe <erik+github@hollensbe.org>
2012-11-12 15:07:05 -0800

    some prison instrumentation to minimize output.

--- lib/chef-workflow/vagrant-helper.rb
+++ lib/chef-workflow/vagrant-helper.rb
@@ -11,7 +11,9 @@ module VagrantHelper
   def vagrant_build_role(role_name, number_of_machines=1)
     IPSupport.singleton.seed_vagrant_ips
 
-    prison = vagrant_prison do
+    $stderr.puts "Bringing up #{number_of_machines} machines for prison #{role_name}"
+
+    prison = vagrant_prison(:ui_class => Vagrant::UI::Silent) do
       configure do |config|
         config.vm.box_url = VagrantSupport.singleton.box_url
         config.vm.box = VagrantSupport.singleton.box
@@ -27,6 +29,8 @@ module VagrantHelper
       vagrant_up
     end
 
+    prison.name = role_name
+
     VagrantSupport.singleton.write_prison(role_name, prison)
     return prison, IPSupport.singleton.get_role_ips(role_name)
   end
@@ -40,6 +44,7 @@ module VagrantHelper
   def vagrant_cleanup(prisons, node_names)
     prisons = [prisons] unless prisons.kind_of?(Array)
     prisons.each do |prison|
+      $stderr.puts "Cleaning up prison for role #{prison.name}"
       prison.cleanup
       VagrantSupport.singleton.remove_prison(prison.name)
     end
