8b257c0fc5bb82ece6beeeea0a3703f5915a8419
Dan Ryan <scriptfu@gmail.com>
2013-01-11 22:55:44 -0500

    read generated knife config when searching

--- lib/chef-workflow/helpers/chef.rb
+++ lib/chef-workflow/helpers/chef.rb
@@ -10,6 +10,7 @@ module ChefHelper
   # Perform a search and return the names of the nodes that match the search.
   #
   def perform_search(type, query)
+  	Chef::Config.from_file(KnifeSupport.singleton.knife_config_path)
     Chef::Search::Query.new.search(type, query).first.map(&:name)
   end
 end
